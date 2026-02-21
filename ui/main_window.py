621be0a69750b77d94af1cfd5326622532059309
javairia772 <javariasaleem39@gmail.com>
2026-02-21 22:45:58 +0500

    feat: Phase 2 - Integrate sorting engine with multiple algorithms

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,7 +1,9 @@
 from PyQt6.QtWidgets import (
     QMainWindow, QWidget, QVBoxLayout, QTableWidget,
-    QTableWidgetItem, QPushButton, QTabWidget
+    QTableWidgetItem, QPushButton, QTabWidget,
+    QHBoxLayout, QComboBox, QLabel
 )
+from sorting.sorting_algorithms import sort_jobs
 from database.db_manager import DatabaseManager
 from utils.dummy_data import generate_dummy_jobs
 
@@ -24,12 +26,42 @@ class MainWindow(QMainWindow):
         tab = QWidget()
         layout = QVBoxLayout()
 
+        # Generate button
         self.generate_btn = QPushButton("Generate Dummy Jobs")
         self.generate_btn.clicked.connect(self.generate_data)
+
+        # Sorting Controls
+        sort_layout = QHBoxLayout()
+
+        self.column_selector = QComboBox()
+        self.column_selector.addItems([
+            "ID", "Title", "Company", "Location",
+            "Salary", "Experience", "Skills",
+            "Job Type", "Posted Date"
+        ])
+
+        self.algorithm_selector = QComboBox()
+        self.algorithm_selector.addItems([
+            "Quick Sort",
+            "Merge Sort",
+            "Bubble Sort",
+            "Tim Sort"
+        ])
+
+        self.sort_btn = QPushButton("Sort")
+        self.sort_btn.clicked.connect(self.sort_data)
+
+        self.time_label = QLabel("Execution Time: 0 ms")
+
+        sort_layout.addWidget(self.column_selector)
+        sort_layout.addWidget(self.algorithm_selector)
+        sort_layout.addWidget(self.sort_btn)
+        sort_layout.addWidget(self.time_label)
 
         self.table = QTableWidget()
 
         layout.addWidget(self.generate_btn)
+        layout.addLayout(sort_layout)
         layout.addWidget(self.table)
 
         tab.setLayout(layout)
@@ -37,18 +69,30 @@ class MainWindow(QMainWindow):
 
     def generate_data(self):
         generate_dummy_jobs(200)
-        self.load_jobs()
-
-    def load_jobs(self):
         jobs = self.db.fetch_all_jobs()
+        self.display_jobs(jobs)
+
+    def display_jobs(self, jobs):
         self.table.setRowCount(len(jobs))
         self.table.setColumnCount(9)
 
         headers = ["ID", "Title", "Company", "Location",
-                   "Salary", "Experience", "Skills", "Job Type", "Posted Date"]
+                "Salary", "Experience", "Skills", "Job Type", "Posted Date"]
 
         self.table.setHorizontalHeaderLabels(headers)
 
         for row_idx, job in enumerate(jobs):
             for col_idx, value in enumerate(job):
                 self.table.setItem(row_idx, col_idx, QTableWidgetItem(str(value)))
+
+
+    def sort_data(self):
+        jobs = self.db.fetch_all_jobs()
+        column = self.column_selector.currentText()
+        algorithm = self.algorithm_selector.currentText()
+
+        sorted_jobs, exec_time = sort_jobs(jobs, column, algorithm)
+
+        self.display_jobs(sorted_jobs)
+
+        self.time_label.setText(f"Execution Time: {round(exec_time*1000, 3)} ms")
